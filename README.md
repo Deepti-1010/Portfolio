937d2ab3d2b3645bbccebe6dea4e7eebc0dc518a
Deepti-1010 <deepumalkani1010@gmail.com>
2025-06-21 21:11:39 +0530

    Add Initial Files

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Portfolio
-This is my portfolio, that contains all my projects
+This is my portfolio, that contains all my projects.
+i am gonna work on it..
---
+++ init.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE student_db;
+
+USE student_db;
+
+CREATE TABLE students (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100),
+    roll_no VARCHAR(20),
+    subject VARCHAR(50),
+    marks INT
+);
